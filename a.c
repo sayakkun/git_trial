1ed55f963d51567d656247a4ac0df9acd44bab7b
sayakkun <sayakhaldar5@gmail.com>
2018-10-08 12:34:19 +0530

    Two files are deleted

--- a.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-	printf("Hello universe");
-	printf("Hello Galaxy");
-	return 0;
-}
--- b.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	int n;
-	printf("Enter a value:");
-	scanf("%d",&n);
-	printf("The value entered: %d\n",n);
-	return 0;
-}
